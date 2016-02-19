de467f7dd4c40b2dc3ee08bcaf69c3902b4957ee
刘祺 <qil@jumei.com>
2016-02-19 18:03:03 +0800

    新增语言翻译、改善第三方插件覆盖Default翻译的问题

--- Han.js
+++ Han.js
@@ -31,9 +31,7 @@ var hanDate = {
 	"Add Open Files": "添加打开的文件",
 	"Add Open Folders": "添加打开的文件夹",
 	"Add Previous Line": "添加至前行",
-	"Arabic": "阿拉伯语 ",
 	"Automatic": "自动编译",
-	"Baltic": "波罗海语 ",
 	"Bookmarks": "书签",
 	"Bookmarks: Clear All": "Bookmarks: Clear All → 书签: 清除所有",
 	"Bookmarks: Select All": "Bookmarks: Select All → 书签: 选择所有",
@@ -43,14 +41,11 @@ var hanDate = {
 	"Bracket Settings – Default": "括号设置 - 默认",
 	"Bracket Settings – User": "括号设置 - 用户",
 	"BracketHighlighter: Toggle Global Enable": "BracketHighlighter: 切换全局启用",
-	"Browse Packages…": "浏览插件",
+	"Browse Packages…": "浏览插件…",
 	"Build Results": "编译结果",
 	"Build System": "编译系统",
-	"Build With…": "运行",
+	"Build With…": "用其编译…",
 	"Cancel Build": "取消编译",
-	"Celtic": "凯尔特语 ",
-	"Central European (ISO 8859-2)": "中欧语 (ISO 8859-2)",
-	"Central European (Windows 1250)": "中欧语 (Windows 1250)",
 	"Changelog": "更新日志",
 	"Changelog…": "更新日志…",
 	"Check for Updates…": "检查更新…",
@@ -96,7 +91,6 @@ var hanDate = {
 	"Copy": "复制",
 	"copy": "复制",
 	"Cut": "剪切",
-	"Cyrillic": "西里尔语 ",
 	"Debug + arguments": "调试 + 参数",
 	"Debug Mode": "调试模式",
 	"Debug": "调试",
@@ -142,7 +136,6 @@ var hanDate = {
 	"Emmet: Update Image Size": "Emmet: Update Image Size → 更新图像大小",
 	"Emmet: Wrap With Abbreviation": "Emmet: Wrap With Abbreviation → 用缩写包裹",
 	"Enter License": "输入许可证",
-	"Estonian": "爱沙尼亚语 ",
 	"Example Key Bindings": "键绑定示例",
 	"Exit": "退出",
 	"Expand Selection to Brackets": "扩展所选括号",
@@ -194,7 +187,6 @@ var hanDate = {
 	"Goto Symbol in Project…": "转至项目中的符号…",
 	"Goto Symbol…": "转到符号…",
 	"Goto": "转到",
-	"Greek": "希腊语 ",
 	"Grid: 4": "网格: 4 画面",
 	"Group 1": "分组 1",
 	"Group 2": "分组 2",
@@ -202,7 +194,6 @@ var hanDate = {
 	"Group 4": "分组 4",
 	"Groups": "分组",
 	"Guess Settings From Buffer": "猜测来自缓冲区的设置",
-	"Hebrew": "希伯来语 ",
 	"Help": "帮助",
 	"Hexadecimal": "十六进制",
 	"HTML: Encode Special Characters": "HTML: Encode Special Characters → HTML: 特殊字符进行编码",
@@ -216,7 +207,6 @@ var hanDate = {
 	"Insert Line After": "插入至行后",
 	"Insert Line Before": "插入至行前",
 	"Install": "安装",
-	"Japanese": "日语 ",
 	"Jump Back": "向后跳转",
 	"Jump Forward": "向前跳转",
 	"Jump to Left Bracket": "跳转到左括号",
@@ -225,7 +215,6 @@ var hanDate = {
 	"Key Bindings – Default": "按键绑定 – 默认",
 	"Key Bindings – Example": "按键绑定 – 示例",
 	"Key Bindings – User": "按键绑定 – 用户",
-	"Korean": "韩语 ",
 	"Larger": "较大",
 	"Layout": "窗口布局",
 	"LICENSE": "许可证",
@@ -258,16 +247,16 @@ var hanDate = {
 	"New Group": "新建分组",
 	"New Plugin…": "新插件…",
 	"New Snippet…": "新代码片段…",
+	"New Syntax…": "新建语法…",
 	"New View into File": "新标签中打开当前文件",
 	"New Window": "新建窗口",
-	"New Workspace for Project": "工作区切换项目",
+	"New Workspace for Project": "为项目新建工作区",
 	"Next Error": "下一个错误",
 	"Next File in Stack": "堆栈中的下一个文件",
 	"Next File": "下一个文件",
 	"Next": "下一个",
 	"No Column Highlights Line": "高亮线不分列",
 	"None": "无",
-	"Nordic": "北欧语 ",
 	"Open Containing Folder…": "打开所在的文件夹…",
 	"Open File": "打开文件",
 	"Open File…": "打开文件…",
@@ -287,6 +276,7 @@ var hanDate = {
 	"Package Control: Discover Packages": "Package Control: Discover Packages → 搜索插件",
 	"Package Control: Enable Package": "Package Control: Enable Package → 启用插件",
 	"Package Control: Grab CA Certs": "Package Control: Grab CA Certs → 获取 CA 证书",
+	"Package Control: Install Local Dependency": "Package Control: Install Local Dependency → 安装本地依赖项",
 	"Package Control: Install Package": "Package Control: Install Package → 安装插件",
 	"Package Control: List Packages": "Package Control: List Packages → 列出插件",
 	"Package Control: List Unmanaged Packages": "Package Control: List Unmanaged Packages → 列出未托管插件",
@@ -294,6 +284,7 @@ var hanDate = {
 	"Package Control: Remove Package": "Package Control: Remove Package → 删除插件",
 	"Package Control: Remove Repository": "Package Control: Remove Repository → 删除资源库",
 	"Package Control: Satisfy Dependencies": "Package Control: Satisfy Dependencies → 满足依赖性",
+	"Package Control: Tests": "Package Control: Tests → 测试",
 	"Package Control: Upgrade Package": "Package Control: Upgrade Package → 升级插件",
 	"Package Control: Upgrade/Overwrite All Packages": "Package Control: Upgrade/Overwrite All Packages → 升级/覆盖所有插件",
 	"Package Settings": "插件设置",
@@ -311,6 +302,7 @@ var hanDate = {
 	"Permute Selections: Sort": "Permute Selections: Sort → 排列选择: 排序",
 	"Permute Selections: Unique": "Permute Selections: Unique → 排列选择: 独特",
 	"Playback Macro": "播放宏",
+	"Plugin Development: Profile Events": "Plugin Development: Profile Events → 插件开发: 事件简报",
 	"Preferences": "首选项",
 	"Preferences: Browse Packages": "Preferences: Browse Packages → 首选项: 浏览程序包",
 	"Preferences: Key Bindings - Default": "Preferences: Key Bindings - Default → 首选项: 键绑定的默认值",
@@ -352,7 +344,6 @@ var hanDate = {
 	"Reveal in Side Bar": "在侧边栏中显示",
 	"Reverse": "反向",
 	"Revert File": "还原文件",
-	"Romanian": "罗马尼亚语 ",
 	"Rot13 Selection": "Rot13 Selection → Rot13 选定",
 	"Rows: 2": "行: 2 窗口",
 	"Rows: 3": "行: 3 窗口",
@@ -399,7 +390,6 @@ var hanDate = {
 	"Show Unsaved Changes…": "显示未保存的更改…",
 	"Shuffle": "无序",
 	"Side Bar": "侧边栏",
-	"Simplified": "-简体 ",
 	"Single": "单窗口",
 	"Smaller": "较小",
 	"Snippets…": "代码片段…",
@@ -417,7 +407,7 @@ var hanDate = {
 	"Swap with Mark": "互换标记",
 	"Switch File": "切换文件",
 	"Switch Header/Implementation": "切换文件头/执行",
-	"Switch Project in Window…": "最近的项目",
+	"Switch Project in Window…": "最近的项目…",
 	"Switch Project…": "切换项目…",
 	"Syntax Specific – User": "特定语法 – 用户",
 	"Syntax": "语法",
@@ -438,8 +428,6 @@ var hanDate = {
 	"Toggle Linter...": "切换检查器…",
 	"Toggle String Bracket Escape Mode": "切换括号内字符串超长隐藏模式",
 	"Tools": "工具",
-	"Traditional": "-繁体 ",
-	"Turkish": "土耳其语 ",
 	"Twitter": "推特",
 	"Undo Selection": "撤销选择",
 	"Unfold All": "展开全部",
@@ -453,7 +441,6 @@ var hanDate = {
 	"UTF-16 BE with BOM": "UTF-16BE 包含BOM",
 	"UTF-16 LE with BOM": "UTF-16 LE 包含BOM",
 	"UTF-8 with BOM": "UTF-8 包含BOM",
-	"Vietnamese": "越南语 ",
 	"View README in Github": "在Github上阅读文档",
 	"View": "查看",
 	"View: Toggle Menu": "View: Toggle Menu → 视图: 切换菜单",
@@ -462,7 +449,6 @@ var hanDate = {
 	"View: Toggle Side Bar": "View: Toggle Side Bar → 视图: 切换侧栏",
 	"View: Toggle Status Bar": "View: Toggle Status Bar → 视图: 切换状态栏",
 	"View: Toggle Tabs": "View: Toggle Tabs → 视图: 切换选项卡",
-	"Western": "西方语 ",
 	"Windows Line Endings (CRLF)": "Windows 换行符 (CRLF)",
 	"Word Wrap Column": "自动换行列",
 	"Word Wrap": "自动换行",
@@ -549,30 +535,74 @@ var commandHan = {
 	},
 };
 
+
+// 数据中无英文文案则按功能翻译
+var langHan = {
+	"Arabic": "阿拉伯文",
+	"Baltic": "波罗的海文",
+	"Celtic": "凯尔特文",
+	"Central European": "中欧",
+	"Chinese Simplified": "简体中文",
+	"Chinese Traditional": "繁体中文",
+	"Cyrillic": "西里尔文",
+	"Estonian": "爱沙尼亚文",
+	"Greek": "希腊文",
+	"Hebrew": "希伯来文",
+	"Japanese": "日文",
+	"Korean": "韩文",
+	"Nordic European": "北欧文",
+	"Nordic": "北欧文",
+	"Romanian": "罗马尼亚语文",
+	"Thai": "泰文",
+	"Turkish": "土耳其文",
+	"Vietnamese": "越南文",
+	"Western European": "西欧",
+	"Western": "西欧",
+};
+
 /**
  * 深度遍历对象进行汉化
  */
-function fixObj(obj) {
+function fixObj(obj, skip) {
 	if (Array.isArray(obj)) {
-		obj.forEach(fixObj);
+		obj.forEach(function(subObj) {
+			fixObj(subObj, skip);
+		});
 	} else if (obj && typeof obj === "object") {
-		var hanCaption = "";
-		if (obj.caption && hanDate[obj.caption]) {
-			hanCaption = hanDate[obj.caption];
-		} else if (obj.command && commandHan[obj.command]) {
-			hanCaption = commandHan[obj.command];
-			if (typeof hanCaption === "object" && obj.args) {
-				for (var key in obj.args) {
-					break;
+		if (skip && obj.id) {
+			// 如果是第三方插件的一级菜单
+			delete obj.caption;
+		} else {
+			// 如果是Sublime本身的一级菜单
+			var hanCaption = "";
+			if (obj.caption && hanDate[obj.caption]) {
+				// 普通文案翻译
+				hanCaption = hanDate[obj.caption];
+			} else if (obj.command && commandHan[obj.command]) {
+				// 按命令翻译
+				hanCaption = commandHan[obj.command];
+				if (typeof hanCaption === "object" && obj.args) {
+					for (var key in obj.args) {
+						break;
+					}
+					hanCaption = hanCaption[obj.args[key]];
 				}
-				hanCaption = hanCaption[obj.args[key]];
+			} else if (obj.caption && obj.args && obj.args.encoding) {
+				// 文语言名称翻译
+				hanCaption = obj.caption.replace(/^([A-Z]+\w+(?:\s+[A-Z]+\w+)*)(\s\(.*?\))$/, function(s, langName, code) {
+					if (langHan[langName]) {
+						return langHan[langName] + code;
+					} else {
+						return s;
+					}
+				});
 			}
-		}
-		if (hanCaption) {
-			if (obj.mnemonic) {
-				hanCaption = hanCaption.replace(/(…?)$/, "(" + obj.mnemonic.toUpperCase() + ")$1");
+			if (hanCaption) {
+				if (obj.mnemonic) {
+					hanCaption = hanCaption.replace(/(…?)$/, "(" + obj.mnemonic.toUpperCase() + ")$1");
+				}
+				obj.caption = hanCaption;
 			}
-			obj.caption = hanCaption;
 		}
 		for (var i in obj) {
 			fixObj(obj[i]);
@@ -597,7 +627,7 @@ function hanJsonFile(data, subPath) {
 		fileCache[filePath] = true;
 		var oldDataJson = JSON.stringify(data, null, '\t');
 
-		var newDataJson = JSON.stringify(fixObj(data), null, '\t');
+		var newDataJson = JSON.stringify(fixObj(data, !/^Default\b/.test(subPath)), null, '\t');
 		if (oldDataJson === newDataJson) {
 
 			// 数据未汉化，不写文件
